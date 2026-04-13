25048feaf92a903f7f49a8d4d8c6658d60671bc6
Pilou44 <guillaume.be@cheerz.com>
2026-04-13 23:37:49 +0200

    wip tests

---
+++ composeApp/src/jvmTest/kotlin/com/wechantloup/spritehandler/importer/AnimationImporterTest.kt
@@ -0,0 +1,42 @@
+package com.wechantloup.spritehandler.importer
+
+import com.wechantloup.spritehandler.exporter.AnimationExporter
+import com.wechantloup.spritehandler.model.Animation
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AnimationImporterTest {
+    @Test
+    fun checkAnimationExportImport() {
+        val frame1 = Random(seed = 42).let { r ->
+            Animation.Frame(
+                spriteFrameIndex = r.nextInt(0, 255),
+                offsetX = r.nextInt(-255, 255),
+                offsetY = r.nextInt(-255, 255),
+            )
+        }
+        val frame2 = Random(seed = 27).let { r ->
+            Animation.Frame(
+                spriteFrameIndex = r.nextInt(0, 255),
+                offsetX = r.nextInt(-255, 255),
+                offsetY = r.nextInt(-255, 255),
+            )
+        }
+        val frame3 = Random(seed = 13).let { r ->
+            Animation.Frame(
+                spriteFrameIndex = r.nextInt(0, 255),
+                offsetX = r.nextInt(-255, 255),
+                offsetY = r.nextInt(-255, 255),
+            )
+        }
+        val sourceFrames = listOf(frame1, frame2, frame3)
+        val sourceAnimation = Animation(sourceFrames)
+
+        val bytes = AnimationExporter.export(sourceAnimation)
+        val resultAnimation = AnimationImporter.import(bytes)
+
+        assertEquals(sourceAnimation.frames, resultAnimation.frames)
+        assertEquals(sourceAnimation, resultAnimation)
+    }
+}
